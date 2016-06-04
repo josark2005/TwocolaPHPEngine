81f5b65eb8845358372eea387bb4670a5234c588
jokin <327928971@qq.com>
2016-06-04 13:49:33 +0800

    v1.4

--- config.inc.php
+++ config.inc.php
@@ -1,7 +1,7 @@
 <?php
 /* 调用以下常量需添加前缀 APP_ */
 $config = array (
-  "MODULE" => "default",  //用于创建MODULE
+  "APP_MODULE" => "default",  //用于创建MODULE
   "TPL_FIX" => ".tpl",
   "SYSTEM_CONSTANT" => true,  //系统常量自检
   "SYSTEM_SUFFIX" => ".html",
--- twocola/Layer/ST.php
+++ twocola/Layer/ST.php
@@ -35,122 +35,122 @@ if(!$storage->FolderExist(APP_PATH)){
     exit();
   }
 }
-if(defined("APP_MODULE")){
-  if(!$storage->FolderExist(APP_PATH."/".APP_MODULE)){
-    if(!$storage->CreateFolder(APP_PATH."/".APP_MODULE)){
-      E("模板文件夹创建失败，请确认目录： ".APP_PATH."/".APP_MODULE." 是否拥有读写权限！");
+if($conf->ConfigExists("APP_MODULE")){
+  if(!$storage->FolderExist(APP_PATH."/".C("APP_MODULE"))){
+    if(!$storage->CreateFolder(APP_PATH."/".C("APP_MODULE"))){
+      E("模板文件夹创建失败，请确认目录： ".APP_PATH."/".C("APP_MODULE")." 是否拥有读写权限！");
       exit();
     }
   }
-  if(!$storage->FileExist(APP_PATH."/".APP_MODULE."/config.inc.php")){
+  if(!$storage->FileExist(APP_PATH."/".C("APP_MODULE")."/config.inc.php")){
     $content = getPresetTpl("config.inc.php");
-    if(!$storage->CreateSave(APP_PATH."/".APP_MODULE."/config.inc.php",$content)){
-      E("应用单独配置文件创建失败，请确认目录： ".APP_PATH."/".APP_MODULE."/config.inc.php"." 是否拥有读写权限！");
+    if(!$storage->CreateSave(APP_PATH."/".C("APP_MODULE")."/config.inc.php",$content)){
+      E("应用单独配置文件创建失败，请确认目录： ".APP_PATH."/".C("APP_MODULE")."/config.inc.php"." 是否拥有读写权限！");
       exit();
     }
   }
-  if(!$storage->FolderExist(APP_PATH."/".APP_MODULE."/View")){
-    if(!$storage->CreateFolder(APP_PATH."/".APP_MODULE."/View")){
-      E("模板文件夹创建失败，请确认目录： ".APP_PATH."/".APP_MODULE."/View"." 是否拥有读写权限！");
+  if(!$storage->FolderExist(APP_PATH."/".C("APP_MODULE")."/View")){
+    if(!$storage->CreateFolder(APP_PATH."/".C("APP_MODULE")."/View")){
+      E("模板文件夹创建失败，请确认目录： ".APP_PATH."/".C("APP_MODULE")."/View"." 是否拥有读写权限！");
       exit();
     }
   }
-  if(!$storage->FolderExist(APP_PATH."/".APP_MODULE."/View/public")){
-    if(!$storage->CreateFolder(APP_PATH."/".APP_MODULE."/View/public")){
+  if(!$storage->FolderExist(APP_PATH."/".C("APP_MODULE")."/View/public")){
+    if(!$storage->CreateFolder(APP_PATH."/".C("APP_MODULE")."/View/public")){
       E("模板公共文件夹创建失败，请确认目录： ".APP_PATH."/View/public"." 是否拥有读写权限！");
       exit();
     }
   }
-  if(!$storage->FolderExist(APP_PATH."/".APP_MODULE."/View/index")){
-    if(!$storage->CreateFolder(APP_PATH."/".APP_MODULE."/View/index")){
-      E("模板公共文件夹创建失败，请确认目录： ".APP_PATH."/".APP_MODULE."/View/index"." 是否拥有读写权限！");
+  if(!$storage->FolderExist(APP_PATH."/".C("APP_MODULE")."/View/index")){
+    if(!$storage->CreateFolder(APP_PATH."/".C("APP_MODULE")."/View/index")){
+      E("模板公共文件夹创建失败，请确认目录： ".APP_PATH."/".C("APP_MODULE")."/View/index"." 是否拥有读写权限！");
       exit();
     }
   }
-  if(!$storage->FolderExist(APP_PATH."/".APP_MODULE."/View/public/html")){
-    if(!$storage->CreateFolder(APP_PATH."/".APP_MODULE."/View/public/html")){
-      E("模板公共文件夹创建失败，请确认目录： ".APP_PATH."/".APP_MODULE."/View/public/html"." 是否拥有读写权限！");
+  if(!$storage->FolderExist(APP_PATH."/".C("APP_MODULE")."/View/public/html")){
+    if(!$storage->CreateFolder(APP_PATH."/".C("APP_MODULE")."/View/public/html")){
+      E("模板公共文件夹创建失败，请确认目录： ".APP_PATH."/".C("APP_MODULE")."/View/public/html"." 是否拥有读写权限！");
       exit();
     }
   }
-  if(!$storage->FolderExist(APP_PATH."/".APP_MODULE."/View/public/static")){
-    if(!$storage->CreateFolder(APP_PATH."/".APP_MODULE."/View/public/static")){
-      E("模板公共文件夹创建失败，请确认目录： ".APP_PATH."/".APP_MODULE."/View/public/static/"." 是否拥有读写权限！");
+  if(!$storage->FolderExist(APP_PATH."/".C("APP_MODULE")."/View/public/static")){
+    if(!$storage->CreateFolder(APP_PATH."/".C("APP_MODULE")."/View/public/static")){
+      E("模板公共文件夹创建失败，请确认目录： ".APP_PATH."/".C("APP_MODULE")."/View/public/static/"." 是否拥有读写权限！");
       exit();
     }
   }
-  if(!$storage->FolderExist(APP_PATH."/".APP_MODULE."/Runtime")){
-    if(!$storage->CreateFolder(APP_PATH."/".APP_MODULE."/Runtime")){
-      E("缓存文件夹创建失败，请确认目录： ".APP_PATH."/".APP_MODULE."/Runtime"." 是否拥有读写权限！");
+  if(!$storage->FolderExist(APP_PATH."/".C("APP_MODULE")."/Runtime")){
+    if(!$storage->CreateFolder(APP_PATH."/".C("APP_MODULE")."/Runtime")){
+      E("缓存文件夹创建失败，请确认目录： ".APP_PATH."/".C("APP_MODULE")."/Runtime"." 是否拥有读写权限！");
     }
   }
   /* Controller */
-  if(!$storage->FolderExist(APP_PATH."/".APP_MODULE."/Controller")){
-    if(!$storage->CreateFolder(APP_PATH."/".APP_MODULE."/Controller")){
-      E("Behavior文件夹创建失败，请确认目录： ".APP_PATH."/".APP_MODULE."/Controller"." 是否拥有读写权限！");
+  if(!$storage->FolderExist(APP_PATH."/".C("APP_MODULE")."/Controller")){
+    if(!$storage->CreateFolder(APP_PATH."/".C("APP_MODULE")."/Controller")){
+      E("Behavior文件夹创建失败，请确认目录： ".APP_PATH."/".C("APP_MODULE")."/Controller"." 是否拥有读写权限！");
       exit();
     }
   }
   /* Behavior */
-  if(!$storage->FolderExist(APP_PATH."/".APP_MODULE."/Controller/Behavior")){
-    if(!$storage->CreateFolder(APP_PATH."/".APP_MODULE."/Controller/Behavior")){
-      E("Behavior文件夹创建失败，请确认目录： ".APP_PATH."/".APP_MODULE."/Controller/Behavior"." 是否拥有读写权限！");
+  if(!$storage->FolderExist(APP_PATH."/".C("APP_MODULE")."/Controller/Behavior")){
+    if(!$storage->CreateFolder(APP_PATH."/".C("APP_MODULE")."/Controller/Behavior")){
+      E("Behavior文件夹创建失败，请确认目录： ".APP_PATH."/".C("APP_MODULE")."/Controller/Behavior"." 是否拥有读写权限！");
       exit();
     }
   }
-  if(!$storage->FileExist(APP_PATH."/".APP_MODULE."/Controller/Behavior/indexBehavior.class.php")){
+  if(!$storage->FileExist(APP_PATH."/".C("APP_MODULE")."/Controller/Behavior/indexBehavior.class.php")){
     $content = getPresetTpl("indexBehavior.class.php");
-    if(!$storage->CreateSave(APP_PATH."/".APP_MODULE."/Controller/Behavior/indexBehavior.class.php",$content)){
-      E("indexBehavior文件创建失败，请确认目录： ".APP_PATH."/".APP_MODULE."/Controller/Behavior/indexBehavior.class.php"." 是否拥有读写权限！");
+    if(!$storage->CreateSave(APP_PATH."/".C("APP_MODULE")."/Controller/Behavior/indexBehavior.class.php",$content)){
+      E("indexBehavior文件创建失败，请确认目录： ".APP_PATH."/".C("APP_MODULE")."/Controller/Behavior/indexBehavior.class.php"." 是否拥有读写权限！");
       exit();
     }
   }
   /* Displayer */
-  if(!$storage->FolderExist(APP_PATH."/".APP_MODULE."/Controller/Displayer")){
-    if(!$storage->CreateFolder(APP_PATH."/".APP_MODULE."/Controller/Displayer")){
-      E("Displayer文件夹创建失败，请确认目录： ".APP_PATH."/".APP_MODULE."/Controller/Displayer"." 是否拥有读写权限！");
+  if(!$storage->FolderExist(APP_PATH."/".C("APP_MODULE")."/Controller/Displayer")){
+    if(!$storage->CreateFolder(APP_PATH."/".C("APP_MODULE")."/Controller/Displayer")){
+      E("Displayer文件夹创建失败，请确认目录： ".APP_PATH."/".C("APP_MODULE")."/Controller/Displayer"." 是否拥有读写权限！");
       exit();
     }
   }
-  if(!$storage->FileExist(APP_PATH."/".APP_MODULE."/Controller/Displayer/indexDisplayer.class.php")){
+  if(!$storage->FileExist(APP_PATH."/".C("APP_MODULE")."/Controller/Displayer/indexDisplayer.class.php")){
     $content = getPresetTpl("indexDisplayer.class.php");
-    if(!$storage->CreateSave(APP_PATH."/".APP_MODULE."/Controller/Displayer/indexDisplayer.class.php",$content)){
-      E("indexDisplayer文件创建失败，请确认目录： ".APP_PATH."/".APP_MODULE."/Controller/Displayer/indexDisplayer.class.php"." 是否拥有读写权限！");
+    if(!$storage->CreateSave(APP_PATH."/".C("APP_MODULE")."/Controller/Displayer/indexDisplayer.class.php",$content)){
+      E("indexDisplayer文件创建失败，请确认目录： ".APP_PATH."/".C("APP_MODULE")."/Controller/Displayer/indexDisplayer.class.php"." 是否拥有读写权限！");
       exit();
     }
   }
   /* BaDCommon */
-  if(!$storage->FolderExist(APP_PATH."/".APP_MODULE."/Controller/Common")){
-    if(!$storage->CreateFolder(APP_PATH."/".APP_MODULE."/Controller/Common")){
-      E("Common文件夹创建失败，请确认目录： ".APP_PATH."/".APP_MODULE."/Controller/Common"." 是否拥有读写权限！");
+  if(!$storage->FolderExist(APP_PATH."/".C("APP_MODULE")."/Controller/Common")){
+    if(!$storage->CreateFolder(APP_PATH."/".C("APP_MODULE")."/Controller/Common")){
+      E("Common文件夹创建失败，请确认目录： ".APP_PATH."/".C("APP_MODULE")."/Controller/Common"." 是否拥有读写权限！");
       exit();
     }
   }
-  if(!$storage->FileExist(APP_PATH."/".APP_MODULE."/Controller/Common/BehaviorCommon.class.php")){
+  if(!$storage->FileExist(APP_PATH."/".C("APP_MODULE")."/Controller/Common/BehaviorCommon.class.php")){
     $content = getPresetTpl("BehaviorCommon.class.php");
-    if(!$storage->CreateSave(APP_PATH."/".APP_MODULE."/Controller/Common/BehaviorCommon.class.php",$content)){
-      E("BehaviorCommon文件创建失败，请确认目录： ".APP_PATH."/".APP_MODULE."/Controller/Common/BehaviorCommon.class.php"." 是否拥有读写权限！");
+    if(!$storage->CreateSave(APP_PATH."/".C("APP_MODULE")."/Controller/Common/BehaviorCommon.class.php",$content)){
+      E("BehaviorCommon文件创建失败，请确认目录： ".APP_PATH."/".C("APP_MODULE")."/Controller/Common/BehaviorCommon.class.php"." 是否拥有读写权限！");
       exit();
     }
   }
   /* Api */
-  if(!$storage->FolderExist(APP_PATH."/".APP_MODULE."/Api")){
-    if(!$storage->CreateFolder(APP_PATH."/".APP_MODULE."/Api")){
-      E("Api文件夹创建失败，请确认目录： ".APP_PATH."/".APP_MODULE."/Api"." 是否拥有读写权限！");
+  if(!$storage->FolderExist(APP_PATH."/".C("APP_MODULE")."/Api")){
+    if(!$storage->CreateFolder(APP_PATH."/".C("APP_MODULE")."/Api")){
+      E("Api文件夹创建失败，请确认目录： ".APP_PATH."/".C("APP_MODULE")."/Api"." 是否拥有读写权限！");
       exit();
     }
   }
-  if(!$storage->FileExist(APP_PATH."/".APP_MODULE."/Api/index.class.php")){
+  if(!$storage->FileExist(APP_PATH."/".C("APP_MODULE")."/Api/index.class.php")){
     $content = getPresetTpl("index.class.php");
-    if(!$storage->CreateSave(APP_PATH."/".APP_MODULE."/Api/index.class.php",$content)){
-      E("Api.class.php文件创建失败，请确认目录： ".APP_PATH."/".APP_MODULE."/Api/index.class.php"." 是否拥有读写权限！");
+    if(!$storage->CreateSave(APP_PATH."/".C("APP_MODULE")."/Api/index.class.php",$content)){
+      E("Api.class.php文件创建失败，请确认目录： ".APP_PATH."/".C("APP_MODULE")."/Api/index.class.php"." 是否拥有读写权限！");
       exit();
     }
   }
-  if(!$storage->FileExist(APP_PATH."/".APP_MODULE."/Controller/Common/Api.class.php")){
+  if(!$storage->FileExist(APP_PATH."/".C("APP_MODULE")."/Controller/Common/Api.class.php")){
     $content = getPresetTpl("Api.class.php");
-    if(!$storage->CreateSave(APP_PATH."/".APP_MODULE."/Controller/Common/Api.class.php",$content)){
-      E("Api.class.php文件创建失败，请确认目录： ".APP_PATH."/".APP_MODULE."/Controller/Common/Api.class.php"." 是否拥有读写权限！");
+    if(!$storage->CreateSave(APP_PATH."/".C("APP_MODULE")."/Controller/Common/Api.class.php",$content)){
+      E("Api.class.php文件创建失败，请确认目录： ".APP_PATH."/".C("APP_MODULE")."/Controller/Common/Api.class.php"." 是否拥有读写权限！");
       exit();
     }
   }
