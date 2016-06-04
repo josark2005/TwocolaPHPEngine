2df0299dcbbe1f3a904df618ac66d33fe7f1a53a
jokin <327928971@qq.com>
2016-06-04 16:01:11 +0800

    v1.5

--- Applications/default/Api/index.class.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Api;
-use TCE\Api;
-class indexAPI extends Api{
-  public function index(){
-    $this->json_e("1","Welcome! You can use our api in this way!","1","0","0");
-  }
-}
-?>
