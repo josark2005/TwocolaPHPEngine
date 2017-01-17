cb977ba3b5259d63d9ec46b1bd7526a79c760023
Jokin <327928971@qq.com>
2017-01-17 18:52:48 +0800

    v3.1

--- index.php
+++ index.php
@@ -5,10 +5,8 @@
  * Library: Twocola Studio
  * ISSUE  : 目前仅支持REWRITE+PATHINFO模式
 */
-define("APP_DEBUG" ,true);                 //调试模式
-// define("APP_DEBUG" ,false);               //调试模式
-define("APP_PATH"  ,"./Applications");    //定义App目录
-define("UpgradeOn" ,false);               //不启动升级模块（推荐保持原样）
-define("URL_MODE"  ,1);                   //不启动升级模块（推荐保持原样）
-require("./twocola/tce.php");     //引入框架引擎
+define("APP_DEBUG" ,true);                 // 调试模式
+define("APP_PATH"  ,"./Applications");    // 定义App目录
+define("URL_MODE"  ,0);                   // URL模式：0兼容  1Pathinfo
+require("./twocola/tce.php");             //引入框架引擎
 ?>
